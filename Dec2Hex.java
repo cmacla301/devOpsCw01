932d207b6822c172206cfa59cfb747a497bbe59f
cmacla301 <167236188+cmacla301@users.noreply.github.com>
2024-11-11 20:32:53 +0000

    Update Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,24 +1,34 @@
 import java.util.Scanner;
 
-class Dec2Hex
-{
-public static int Arg1;
-    public static void main(String args[])    {
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+class Dec2Hex {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("Error: No input argument provided. Please provide an integer to convert.");
+            return;
+        }
 
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
+        int num;
+        try {
+            num = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.err.println("Error: Non-integer input detected. Please provide a valid integer.");
+            return;
+        }
+
+        char[] hexDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        String hexadecimal = "";
+
+        if (num == 0) {
+            hexadecimal = "0";
+        } else {
+            int tempNum = num;
+            while (tempNum != 0) {
+                int rem = tempNum % 16;
+                hexadecimal = hexDigits[rem] + hexadecimal;
+                tempNum = tempNum / 16;
+            }
         }
 
         System.out.println("Hexadecimal representation is: " + hexadecimal);
-        System.out.println("hello world " + hexadecimal);
     }
 }
